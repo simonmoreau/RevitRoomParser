61fbb65a6941de7f4ac3b5711601404e8b6126d9
Simon Moreau <simon@bim42.com>
2021-01-03 23:18:08 +0100

    export room boundaries to a svg

--- RevitRoomParserBundle/RevitRoomParserBundle/RevitRoomParserBundleApp.cs
+++ RevitRoomParserBundle/RevitRoomParserBundle/RevitRoomParserBundleApp.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Svg;
 
 namespace RevitRoomParserBundle
 {
@@ -46,16 +47,78 @@ namespace RevitRoomParserBundle
 
             List<string> roomNames = new List<string>();
 
+            SvgDocument svgDoc = new SvgDocument
+            {
+                Width = 20,
+                Height = 20,
+                ViewBox = new SvgViewBox(-10, -10, 20, 20),
+            };
+
+            BoundingBox boundingBox = new BoundingBox();
+
             foreach (Autodesk.Revit.DB.Architecture.Room room in rooms)
             {
                 if (room != null)
                 {
                     roomNames.Add(room.Name);
+
+                    SpatialElementBoundaryOptions opt = new SpatialElementBoundaryOptions();
+                    opt.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Finish;
+
+                    IList<IList<Autodesk.Revit.DB.BoundarySegment>> boundarySegmentArray = room.GetBoundarySegments(opt);
+                    if (null == boundarySegmentArray)  //the room may not be bound
+                    {
+                        continue;
+                    }
+
+                    foreach (IList<Autodesk.Revit.DB.BoundarySegment> boundarySegArr in boundarySegmentArray)
+                    {
+                        if (0 == boundarySegArr.Count)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            foreach (Autodesk.Revit.DB.BoundarySegment boundarySegment in boundarySegArr)
+                            {
+                                //Check if the boundary is a room separation lines
+                                Element boundaryElement = doc.GetElement(boundarySegment.ElementId);
+
+                                if (boundaryElement == null) { continue; }
+
+                                Categories categories = doc.Settings.Categories;
+                                Category RoomSeparetionLineCat = categories.get_Item(BuiltInCategory.OST_RoomSeparationLines);
+
+                                if (boundaryElement.Category.Id != RoomSeparetionLineCat.Id)
+                                {
+
+                                    Curve boundaryCurve = boundarySegment.GetCurve();
+
+                                    if (boundaryCurve != null)
+                                    {
+                                        svgDoc.Children.Add(SvgConversion.ConvertCurve(boundaryCurve));
+                                        boundingBox.UpdateBox(boundaryCurve.GetEndPoint(0));
+                                        boundingBox.UpdateBox(boundaryCurve.GetEndPoint(1));
+                                    }
+
+                                }
+                            }
+                        }
+                    }
                 }
             }
 
-            string path = @".\rooms.csv";
-            File.WriteAllLines(path, roomNames.ToArray());
+            svgDoc.Width = new SvgUnit((float)boundingBox.GetWidth());
+            svgDoc.Height = new SvgUnit((float)boundingBox.GetHeight());
+            svgDoc.ViewBox = new SvgViewBox((float)boundingBox.minX, (float)boundingBox.minY, (float)boundingBox.GetWidth(), (float)boundingBox.GetHeight());
+
+
+
+            string path = @".\rooms.svg";
+            // File.WriteAllLines(path, roomNames.ToArray());
+
+            svgDoc.Write(path);
+
 
             LogTrace("Rooms file saved in the working directory");
 
@@ -66,5 +129,57 @@ namespace RevitRoomParserBundle
         /// This will appear on the Design Automation output
         /// </summary>
         private static void LogTrace(string format, params object[] args) { System.Console.WriteLine(format, args); }
+    }
+
+    public class BoundingBox
+    {
+        public BoundingBox()
+        {
+            minX = 0;
+            minY = 0;
+            maxX = 0;
+            maxY = 0;
+            margin = 10;
+
+
+        }
+        public double minX { get; set; }
+        public double maxX { get; set; }
+        public double minY { get; set; }
+        public double maxY { get; set; }
+        public double margin { get; set; }
+
+        public void UpdateBox(XYZ point)
+        {
+            if (minX > point.X)
+            {
+                minX = point.X - margin;
+            }
+
+            if(maxX < point.X)
+            {
+                maxX = point.X + margin;
+            }
+
+            if (minY > point.Y)
+            {
+                minY = point.Y - margin;
+            }
+
+            if(maxY < point.Y)
+            {
+                maxY = point.Y + margin;
+            }
+        }
+
+        public double GetWidth()
+        {
+            return maxX - minX;
+        }
+
+        public double GetHeight()
+        {
+            return maxY - minY;
+        }
     }
 }
---
+++ RevitRoomParserBundle/RevitRoomParserBundle/SvgConversion.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Svg;
+using SVG = Svg;
+using Svg.Pathing;
+using Autodesk.Revit.DB;
+
+namespace RevitRoomParserBundle
+{
+    static class SvgConversion
+    {
+        public static SvgVisualElement ConvertCurve(Curve curve)
+        {
+            SvgVisualElement svgVisualElement = null;
+            if (curve is Arc)
+            {
+                svgVisualElement = ConvertArc((Arc)curve);
+            }
+            else if (curve is Line)
+            {
+                svgVisualElement = ConvertLine((Line)curve);
+            }
+
+            svgVisualElement.Stroke = new SvgColourServer(System.Drawing.Color.Black);
+            svgVisualElement.StrokeWidth = new SvgUnit((float)0.2);
+            svgVisualElement.Fill = SvgPaintServer.None;
+
+            return svgVisualElement;
+
+        }
+
+        private static SvgPath ConvertArc(Arc arc)
+        {
+
+            SvgPath svgPath = new SvgPath();
+
+            SvgPathSegmentList svgPathSegmentList = new SvgPathSegmentList();
+            svgPathSegmentList.Add(new SvgMoveToSegment(arc.GetEndPoint(0).ConvertToPointF()));
+
+            XYZ a = arc.GetEndPoint(0) - arc.Center;
+            XYZ b = arc.GetEndPoint(1) - arc.Center;
+
+            double angleAboutAxis = a.AngleOnPlaneTo(b, arc.Normal);
+
+
+            SvgArcSweep svgArcSweep = SvgArcSweep.Positive;
+
+            if (angleAboutAxis >= 180) { svgArcSweep = SvgArcSweep.Negative; }
+
+            SvgArcSize svgArcSize = SvgArcSize.Small;
+
+            SvgArcSegment svgArcSegment = new SvgArcSegment(svgPathSegmentList.Last.End, (float)arc.Radius, (float)arc.Radius, (float)angleAboutAxis, svgArcSize, svgArcSweep, arc.GetEndPoint(1).ConvertToPointF());
+
+            svgPathSegmentList.Add(svgArcSegment);
+            svgPath.PathData = svgPathSegmentList;
+
+            return svgPath;
+
+        }
+
+        private static SvgPath ConvertLine(Line line)
+        {
+
+            SvgPath svgPath = new SvgPath();
+
+            SvgPathSegmentList svgPathSegmentList = new SvgPathSegmentList();
+
+            svgPathSegmentList.Add(new SvgMoveToSegment(line.GetEndPoint(0).ConvertToPointF()));
+
+            SvgLineSegment svgLineSegment = new SvgLineSegment(svgPathSegmentList.Last.End, line.GetEndPoint(1).ConvertToPointF());
+
+            svgPathSegmentList.Add(svgLineSegment);
+            svgPath.PathData = svgPathSegmentList;
+
+
+            return svgPath;
+
+        }
+
+
+        private static System.Drawing.PointF ConvertToPointF(this XYZ point)
+        {
+            System.Drawing.PointF pointF = new System.Drawing.PointF((float)point.X, -(float)point.Y);
+
+            return pointF;
+        }
+
+    }
+}
