b227ca786f0dd70855a712cc01f080f2f6d8b895
Simon Moreau <simon@bim42.com>
2021-01-04 14:45:57 +0100

    add angular material

--- RevitRoomParserApp/src/app/app.component.ts
+++ RevitRoomParserApp/src/app/app.component.ts
@@ -2,8 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
   value = 'World';
+  isIframe = true;
+  loggedIn = false;
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+
 }
--- RevitRoomParserApp/src/app/app.module.ts
+++ RevitRoomParserApp/src/app/app.module.ts
@@ -1,11 +1,31 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from "./app.component";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule {}
